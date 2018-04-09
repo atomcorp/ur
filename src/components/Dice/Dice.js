b55270aba843565056bef8f9219a4b9aa9692cc4
atomcorp <thomasmaxwellsmith@gmail.com>
2018-04-09 20:53:45 +0100

    working config for Flow (please D:)

--- src/components/Dice/Dice.js
+++ src/components/Dice/Dice.js
@@ -2,20 +2,28 @@
 import React from 'react';
 import tailsImg from '../../assets/die-tails.png';
 import headsImg from '../../assets/die-heads.png';
+import type {DieType} from './Dice.types.js';
 
 /*
   Returns one Die, either positive or negative, default positive
 */
-const Die = (
-  face = 'tails'
-) => {
+const Die = ({
+  face,
+}: DieType) => {
   return (
     <div>
-      <img src={face === 'heads' ? tailsImg : headsImg} alt=""/>
+      <img src={face === 'heads' ? headsImg : tailsImg} alt=""/>
     </div>
   );
 };
 
-const Dice = 1;
+const Dice = () => (
+  <div>
+    <Die face={'heads'} key={1} />;
+    <Die face={'tails'} key={2} />;
+    <Die face={'heads'} key={3} />;
+    <Die face={'tails'} key={4} />;
+  </div>
+);
 
-export default Die;
+export default Dice;
