239deefff6577b7a9937887e9ec91ce68e3de46d
atomcorp <thomasmaxwellsmith@gmail.com>
2018-04-28 21:19:29 +0100

    refactored out non-moves to new file

--- src/methods/game.js
+++ src/methods/game.js
@@ -1,3 +1,6 @@
+import {
+  ACTION_CREATORS,
+} from '../redux/actions';
 /**
  * This will contain all the game logic
  *
@@ -11,3 +14,129 @@
  *   - handle who's turn it is
  *   - handle whether you can throw the dice
  */
+
+
+const isProposedSquareOccupied = (counter, store, proposedSquareId) => {
+  if (store.board[proposedSquareId].contents.length > 0) {
+    return true;
+  }
+  return false;
+};
+
+const isProposedSquareOccupiedWithOwnCounter = (squareContents, counter) => {
+  return squareContents.some(
+    (content) => content.playerId === counter.playerId
+  );
+};
+
+const isProposedSquareOccupiedWithOpponentOnRosette = (square, counter) => {
+  const doesOpponentOccupy = square.contents.some(
+    (content) => content.playerId !== counter.playerId
+  );
+  if (doesOpponentOccupy && square.isRosette) {
+    return true;
+  }
+  return false;
+};
+
+const doesProposedSquareExist = (moves, trackNumber) => {
+  return moves + trackNumber > 15 ? true : false;
+};
+
+const isProposedSquareAnIllegalMove = (
+  counter,
+  store,
+  dispatch,
+  proposedSquareId,
+) => {
+  // is square already occupied with own players counter
+  // is square occupied with another players counter on a rosette
+  // is counter players?
+  // does proposed square exist? ---
+  const squareDoesntExist = doesProposedSquareExist(
+    store.dice.moves,
+    store.board[counter.squareId].trackNumber
+  );
+  if (squareDoesntExist) {
+    dispatch(ACTION_CREATORS.showGameMessage({
+      message: 'That square doesn\'t exist! You need an exact roll.',
+    }));
+    return true;
+  }
+
+  const isOccupiedWithOwnCounter =
+    isProposedSquareOccupied(counter, store, proposedSquareId) &&
+    isProposedSquareOccupiedWithOwnCounter(
+      store.board[proposedSquareId].contents,
+      counter
+    );
+  if (isOccupiedWithOwnCounter) {
+    dispatch(ACTION_CREATORS.showGameMessage({
+      message: 'Square already has your counter in it',
+    }));
+  }
+  const isOccupiedWithOtherPlayersCounterOnARosette =
+    isProposedSquareOccupied(counter, store, proposedSquareId) &&
+    isProposedSquareOccupiedWithOpponentOnRosette(
+      store.board[proposedSquareId],
+      counter
+    );
+  if (isOccupiedWithOtherPlayersCounterOnARosette) {
+    dispatch(ACTION_CREATORS.showGameMessage({
+      message: 'You can\'t take an opponents counter on a rosette',
+    }));
+  }
+  if (
+    squareDoesntExist ||
+    isOccupiedWithOwnCounter ||
+    isOccupiedWithOtherPlayersCounterOnARosette
+  ) {
+    return true;
+  }
+  return false;
+};
+
+
+export const canCounterBeClicked = (
+  counter,
+  dispatch,
+  store,
+  proposedSquareId
+) => {
+  const hasPlayerClickedOpponentCounter
+    = counter.playerId !== store.turn.playersTurn;
+  const playerHasntRolledDice = store.turn.canRollDice;
+  const playersTurnHasEnded = store.turn.turnEnded;
+  const proposedSquareCantBeUsed = isProposedSquareAnIllegalMove(
+    counter, store, dispatch, proposedSquareId
+  );
+  if (hasPlayerClickedOpponentCounter) {
+    dispatch(ACTION_CREATORS.showGameMessage({
+      message: 'That\'s not your counter!',
+    }));
+  }
+  if (playerHasntRolledDice) {
+    dispatch(ACTION_CREATORS.showGameMessage({
+      message: 'You need to roll the dice!',
+    }));
+  }
+  if (playersTurnHasEnded) {
+    dispatch(ACTION_CREATORS.showGameMessage({
+      message: 'Your turn has ended',
+    }));
+  }
+  if (proposedSquareCantBeUsed) {
+    dispatch(ACTION_CREATORS.showGameMessage({
+      message: 'You can\'t move to that square!',
+    }));
+  }
+  if (
+    hasPlayerClickedOpponentCounter
+    || playerHasntRolledDice
+    || playersTurnHasEnded
+    || proposedSquareCantBeUsed
+  ) {
+    return false;
+  }
+  return true;
+};
--- src/redux/actions/counter-actions.js
+++ src/redux/actions/counter-actions.js
@@ -2,6 +2,7 @@
 import {ACTION_TYPES} from './index';
 import {ACTION_CREATORS} from './';
 import type {MoveCountersType} from '../reducers/reducers.types';
+import {canCounterBeClicked} from '../../methods/game';
 import {helpCalculateSquareId} from '../../methods/helpers';
 
 const updateCounter = ({counterId, squareId}: MoveCountersType) => ({
@@ -16,126 +17,6 @@ const clickedCounter = ({
   type: ACTION_TYPES.CLICKED_ON_COUNTER_ATTEMPT,
   counter,
 });
-
-const isProposedSquareOccupied = (counter, store, proposedSquareId) => {
-  if (store.board[proposedSquareId].contents.length > 0) {
-    return true;
-  }
-  return false;
-};
-
-const isProposedSquareOccupiedWithOwnCounter = (squareContents, counter) => {
-  return squareContents.some(
-    (content) => content.playerId === counter.playerId
-  );
-};
-
-const isProposedSquareOccupiedWithOpponentOnRosette = (square, counter) => {
-  const doesOpponentOccupy = square.contents.some(
-    (content) => content.playerId !== counter.playerId
-  );
-  if (doesOpponentOccupy && square.isRosette) {
-    return true;
-  }
-  return false;
-};
-
-const doesProposedSquareExist = (moves, trackNumber) => {
-  return moves + trackNumber > 15 ? true : false;
-};
-
-const isProposedSquareAnIllegalMove = (
-  counter,
-  store,
-  dispatch,
-  proposedSquareId,
-) => {
-  // is square already occupied with own players counter
-  // is square occupied with another players counter on a rosette
-  // is counter players?
-  // does proposed square exist? ---
-  const squareDoesntExist = doesProposedSquareExist(
-    store.dice.moves,
-    store.board[counter.squareId].trackNumber
-  );
-  if (squareDoesntExist) {
-    dispatch(ACTION_CREATORS.showGameMessage({
-      message: 'That square doesn\'t exist! You need an exact roll.',
-    }));
-    return true;
-  }
-
-  const isOccupiedWithOwnCounter =
-    isProposedSquareOccupied(counter, store, proposedSquareId) &&
-    isProposedSquareOccupiedWithOwnCounter(
-      store.board[proposedSquareId].contents,
-      counter
-    );
-  if (isOccupiedWithOwnCounter) {
-    dispatch(ACTION_CREATORS.showGameMessage({
-      message: 'Square already has your counter in it',
-    }));
-  }
-  const isOccupiedWithOtherPlayersCounterOnARosette =
-    isProposedSquareOccupied(counter, store, proposedSquareId) &&
-    isProposedSquareOccupiedWithOpponentOnRosette(
-      store.board[proposedSquareId],
-      counter
-    );
-  if (isOccupiedWithOtherPlayersCounterOnARosette) {
-    dispatch(ACTION_CREATORS.showGameMessage({
-      message: 'You can\'t take an opponents counter on a rosette',
-    }));
-  }
-  if (
-    squareDoesntExist ||
-    isOccupiedWithOwnCounter ||
-    isOccupiedWithOtherPlayersCounterOnARosette
-  ) {
-    return true;
-  }
-  return false;
-};
-
-
-const canCounterBeClicked = (counter, dispatch, store, proposedSquareId) => {
-  const hasPlayerClickedOpponentCounter
-    = counter.playerId !== store.turn.playersTurn;
-  const playerHasntRolledDice = store.turn.canRollDice;
-  const playersTurnHasEnded = store.turn.turnEnded;
-  const proposedSquareCantBeUsed = isProposedSquareAnIllegalMove(
-    counter, store, dispatch, proposedSquareId
-  );
-  if (hasPlayerClickedOpponentCounter) {
-    dispatch(ACTION_CREATORS.showGameMessage({
-      message: 'That\'s not your counter!',
-    }));
-  }
-  if (playerHasntRolledDice) {
-    dispatch(ACTION_CREATORS.showGameMessage({
-      message: 'You need to roll the dice!',
-    }));
-  }
-  if (playersTurnHasEnded) {
-    dispatch(ACTION_CREATORS.showGameMessage({
-      message: 'Your turn has ended',
-    }));
-  }
-  if (proposedSquareCantBeUsed) {
-    dispatch(ACTION_CREATORS.showGameMessage({
-      message: 'You can\'t move to that square!',
-    }));
-  }
-  if (
-    hasPlayerClickedOpponentCounter
-    || playerHasntRolledDice
-    || playersTurnHasEnded
-    || proposedSquareCantBeUsed
-  ) {
-    return false;
-  }
-  return true;
-};
 
 // need to simplify the logic going in here
 export const clickedOnCounter = (counter) => {
