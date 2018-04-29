2976db6e8276aa9dc715b5673287480fa6d35509
atomcorp <faotms@gmail.com>
2018-04-29 16:29:47 +0100

    basic score counter

--- src/containers/GameStatus/GameStatus.js
+++ src/containers/GameStatus/GameStatus.js
@@ -2,18 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
-const GameStatus = ({game}) => (
+const GameStatus = ({
+  game,
+  playerAStart,
+  playerAEnd,
+  playerBStart,
+  playerBEnd,
+}) => (
   <div>
     Game status: {game.inProgress ? 'Started' : 'Not started'}
     <br />
     Messages: {
       game.message.map((message) => message).join(', ')
     }
+      playerA Start: {playerAStart}<br />
+      playerA End: {playerAEnd}<br />
+      ---<br />
+      playerB Start: {playerBStart}<br />
+      playerB End: {playerBEnd}<br />
   </div>
 );
 
 const mapStateToProps = (store) => ({
   game: store.game,
+  playerAStart: store.board['playerA-0'].contents.length,
+  playerAEnd: store.board['playerA-15'].contents.length,
+  playerBStart: store.board['playerB-0'].contents.length,
+  playerBEnd: store.board['playerB-15'].contents.length,
 });
 
 GameStatus.propTypes = {
